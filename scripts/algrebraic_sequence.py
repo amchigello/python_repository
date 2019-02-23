3b4a66f5d974f917093cc9e6dbecde8029065815
nayak <unpandu104@gmail.com>
2019-02-23 14:31:36 +0530

    format

--- scripts/algrebraic_sequence.py
+++ scripts/algrebraic_sequence.py
@@ -1,11 +1,13 @@
 
-def geometric_progression(a,r,n):
-    x=[a*(r**i) for i in range(1,n+1)]
+def geometric_progression(a, r, n):
+    x = [a*(r**i) for i in range(1, n+1)]
     print(x)
-    
-def arithmetic_progression(a,d,n):
-    x=[a+(i-1)*d for i in range(1,n+1)]
-    print(x)    
-    
-geometric_progression(1,2,10)
-arithmetic_progression(1,2,10)
+
+
+def arithmetic_progression(a, d, n):
+    x = [a+(i-1)*d for i in range(1, n+1)]
+    print(x)
+
+
+geometric_progression(1, 2, 10)
+arithmetic_progression(1, 2, 10)
