3384d940847fa5bcc4e5376a520bea09d5cf18df
nayak <unpandu104@gmail.com>
2019-03-08 21:02:03 +0530

    add

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,11 @@
-from functools import reduce
-lst=list(range(5))
-x=reduce(lambda x,y:x*y,lst)
+import copy
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]
+     ]
+c = copy.deepcopy(a)
+
+for i in range(len(a)):
+    for j in range(len(c)):
+        c[i][j] = a[j][i]
+
+print(a)
+print(c)
