72b092ccc6dc04ed3237e5a0179459dcdd40dfc7
amchigello <42021309+amchigello@users.noreply.github.com>
2020-04-25 19:20:07 +0530

    Update plot.py

--- matplotlib_work/plot.py
+++ matplotlib_work/plot.py
@@ -2,27 +2,29 @@ from matplotlib import pyplot as plt
 import requests
 
 
-def get_response(url):
+def get_country_timeline(country_code):
+    url = "https://thevirustracker.com/free-api?countryTimeline={}".format(
+        country_code)
     response = requests.get(url).json()
+    country = response["countrytimelinedata"][0]["info"]["title"]
     data = response["timelineitems"][0]
     del data['stat']
     dev_x = list(data.keys())
     dev_y = [x['new_daily_cases'] for x in list(data.values())]
-    return (dev_x, dev_y)
+    return (dev_x, dev_y, country)
 
-
-if __name__ == "__main__":
-    india_data = get_response(
-        "https://thevirustracker.com/free-api?countryTimeline=IN")
-    us_data = get_response(
-        "https://thevirustracker.com/free-api?countryTimeline=KR")
-
-    plt.plot(range(len(india_data[1])), india_data[1], label="India", color="#10a6c4")
-    plt.plot(range(len(us_data[1])), us_data[1], label="South Korea", color="#97c410")
+def plot_daily_cases(*countries):
+    for country in countries:
+        data = get_country_timeline(country)
+        plt.plot(range(len(data[1])), data[1], label=data[2])
     plt.legend()
     plt.grid()
     plt.title("Daily Coronavirus count")
     plt.xlabel("Days since first case")
     plt.ylabel("Case count per day")
     plt.show()
+    return True
 
+
+if __name__ == "__main__":
+    plot_daily_cases("IN","GB")
