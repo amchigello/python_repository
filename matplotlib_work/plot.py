33a1715fa113d1828abb7313968a237c0631f26c
nayak <unpandu104@gmail.com>
2020-06-04 12:11:35 +0530

    -

--- matplotlib_work/plot.py
+++ matplotlib_work/plot.py
@@ -1,5 +1,4 @@
-import re
-file_path = "Order_Reports__2017__2020_-_In_05-05-2020-12-15-09"
-pattern = re.compile('\d{2}-\d{2}-\d{4}')
-date = re.findall(pattern, file_path)
-print(date[0])
+import requests
+url="https://api.covid19india.org/states_daily.json"
+response = requests.get(url).json()
+print(response)
